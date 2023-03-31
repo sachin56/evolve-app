4a8d393a3ffe5593a7b23b5be7ab358cd133adf7
xShamilAx <shamila.e.c.f@gmail.com>
2023-03-31 12:51:15 +0530

    changes

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,12 +1,5 @@
     <!-- need to remove -->
 
-
-    <li class="nav-item">
-        <a href="/home" class="nav-link dashboard_route">
-            <i class="nav-icon fas fa-home"></i>
-            <p>Dashboard</p>
-        </a>
-    </li>
     <li class="nav-item">
         <a href="/seller" class="nav-link dashboard_route">
             <i class="fas fa-object-ungroup"></i>
